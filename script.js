da4666aa1a22234b60067a2d2e2934e27226196a
ankushh19 <ak9108824@gmail.com>
2024-06-10 23:53:35 +0530

    media-page

---
+++ script.js
@@ -0,0 +1,10 @@
+function playVideo() {
+  var playButton = document.querySelector(".play-btn");
+  var thumbnail = document.querySelector(".video-thumbnail");
+  var video = document.getElementById("youtube-video");
+
+  playButton.style.display = "none";
+  thumbnail.style.display = "none";
+  video.style.display = "block";
+  video.src += "&autoplay=1";
+}
